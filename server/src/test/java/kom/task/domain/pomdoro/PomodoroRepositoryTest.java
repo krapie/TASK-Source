965498f36135ec557682c734ff0cf1bbfad86d35
Komputer-P <wlghks0314@gmail.com>
2021-05-22 20:57:04 +0900

    2021-05-22

---
+++ server/src/test/java/kom/task/domain/pomdoro/PomodoroRepositoryTest.java
@@ -0,0 +1,50 @@
+package kom.task.domain.pomdoro;
+
+import kom.task.domain.pomodoro.Pomodoro;
+import kom.task.domain.pomodoro.PomodoroRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PomodoroRepositoryTest {
+
+    @Autowired
+    PomodoroRepository pomodoroRepository;
+
+    @After
+    public void cleanup() {
+        pomodoroRepository.deleteAll();
+    }
+
+    @Test
+    public void pomodoro_repository_save_test() {
+        //given
+        String userId = "1234";
+        Integer timerSet = 25*60;
+        Integer pomo = 0;
+
+        pomodoroRepository.save(Pomodoro.builder()
+                .userId(userId)
+                .timerSet(timerSet)
+                .pomo(pomo)
+                .build());
+
+        //when
+        List<Pomodoro> pomodoroList = pomodoroRepository.findAll();
+
+        //then
+        Pomodoro pomodoro = pomodoroList.get(0);
+        assertThat(pomodoro.getUserId()).isEqualTo(userId);
+        assertThat(pomodoro.getTimerSet()).isEqualTo(timerSet);
+        assertThat(pomodoro.getPomo()).isEqualTo(pomo);
+    }
+}
